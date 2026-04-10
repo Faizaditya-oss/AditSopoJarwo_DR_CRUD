3dc74940afe4980aebf54df16c1bf6dd13833faa
Faizaditya-oss <faizaditya750@gmail.com>
2026-04-10 15:31:04 +0700

    Menampilkan Data ke Form Saat Baris DIpilih.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -200,6 +200,7 @@
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(525, 213);
             this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // btnInsert_Click
             // 
